88178a29a9dcfdeff9a86ea38223ae1a0a442a28
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2019-08-28 20:13:10 +0530

    Update ProductController.php

--- app/Api/V1/Controllers/Master/ProductController.php
+++ app/Api/V1/Controllers/Master/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Api\V1\Controllers\Master;
 
+use App\Models\Master\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,14 +10,37 @@ class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     * @param $items
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($items = 10)
     {
-        //
+        $products = $this->search()->paginate($items);
+
+        return response()->json([
+            'status'=>true,
+            'data'=>$products,
+            'message'=>"Products List paginated"
+        ],200);
     }
 
+    function search(){
+        $product = Product::query();
+        $search = \Request::get('search');
+        
+        if ($search !== null && $search !=='')
+        {
+            foreach ($search as $param=>$key)
+            {
+
+                if ($key !=='' && $key !==null && $key !=="undefined")
+                {
+                    $product = $product->Orwhere($param,'LIKE',"%".$key."%");
+                }
+            }
+        }
+        return $product;
+    }
     /**
      * Show the form for creating a new resource.
      *
