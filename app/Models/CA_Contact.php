c0cb1b3ebb2198079b2ab81911561e6ace957a6f
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2019-09-29 13:25:27 +0530

    Added Fillable Property

--- app/Models/CA_Contact.php
+++ app/Models/CA_Contact.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class CA_Contact extends Model
 {
     protected $table= 'ca_contacts';
+    protected $guarded=[];
+    protected $fillable = [
+        'ca_contact_first_name',
+        'ca_contact_last_name',
+        'ca_contact_email',
+        'ca_contact_mobile_number',
+        'ca_contact_alternate_mobile_number',
+        'ca_contact_designation',
+        'ca_contact_branch',
+    ];
+
+
 }
